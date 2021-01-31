b4b798f0eb8d29cb6fc2ef4a84ada6deff855289
Artem <skrartrom@icloud.com>
2021-01-31 16:11:13 +0200

    Logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-  get 'sign_up', to: 'registrations#new'
+  # User
+  get  'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+
+  delete 'logout',  to: 'sessions#destroy'
+  #
 end
