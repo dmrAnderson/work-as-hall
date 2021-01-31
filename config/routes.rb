06e2b4dfd65d700292f57b71b3e965497f37452f
Artem <skrartrom@icloud.com>
2021-01-31 15:23:11 +0200

    Sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
 end
