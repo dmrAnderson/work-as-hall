eafb950278e3a6722ea4465fec3b56065c7efde0
Artem <skrartrom@icloud.com>
2021-01-31 23:26:37 +0200

    Edit Password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  before_action :require_user_logged_in!, except: :destroy
+
   def new; end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   get  'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
 
-  get 'sign_in', to: 'sessions#new'
-  post 'sign_in', to: 'sessions#create'
+  get    'sign_in', to: 'sessions#new'
+  post   'sign_in', to: 'sessions#create'
   delete 'logout',  to: 'sessions#destroy'
+
+  get   'password', to: 'passwords#edit'
+  patch 'password', to: 'passwords#update'
   #
 end
