faec35aa4005e0e4ed0a790f6a056e3b307a79ad
Artem <skrartrom@icloud.com>
2021-01-31 14:27:27 +0200

    Flash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+    flash.now[:notice] = 'Logged in successfully'
+    flash.now[:alert] = 'Invalid email or password'
+  end
 
   def about; end
 end
