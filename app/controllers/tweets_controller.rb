9eac8300fe9987a109b61791709d8b3a9bf5da73
Artem <skrartrom@icloud.com>
2021-02-09 01:02:30 +0200

    Tweet Validations

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,11 +6,21 @@ class TweetsController < ApplicationController
   end
 
   def new
-    # @tweets = Current.user.tweets.new
     @tweet = Tweet.new
   end
 
   def create
-    
+    @tweets = Current.user.tweets.new(tweet_params)
+    if @tweets.save
+      redirect_to(:tweets, notice: 'Tweet was scheduled successfully')
+    else
+      render(:new)
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_acount_id, :body, :publish_at)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,9 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, presence: true, length: { maximum: 280 }
+  validates :publish_at, presence: true
+
+  after_initialize -> { self.publish_at ||= 24.hour.from_now }
 end
