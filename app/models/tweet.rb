ce91e048deff36cbe3457aabc7172448eccbb09e
Artem <skrartrom@icloud.com>
2021-02-10 00:23:27 +0200

    Background Job for Posting Tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,6 +6,11 @@ class Tweet < ApplicationRecord
   validates :publish_at, presence: true
 
   after_initialize -> { self.publish_at ||= 24.hour.from_now }
+  after_save_commit do
+    if published_at_previously_changed?
+      TweetJob.set(wait_until: @tweet.publish_at).perform_later(self)
+    end
+  end
 
   def published?
     tweet_id?
