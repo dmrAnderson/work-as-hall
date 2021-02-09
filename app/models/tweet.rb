fe6ab4ae040f087344262d40b702eabd35acef7e
Artem <skrartrom@icloud.com>
2021-02-09 23:17:04 +0200

    Tweet Partial

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,4 +6,8 @@ class Tweet < ApplicationRecord
   validates :publish_at, presence: true
 
   after_initialize -> { self.publish_at ||= 24.hour.from_now }
+
+  def published?
+    tweet_id?
+  end
 end
